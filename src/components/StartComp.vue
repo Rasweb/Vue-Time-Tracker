433a644884e571cf74a808c5fe7a6859dde66370
Rasmus <Rasmuspalm@fastmail.se>
2023-01-23 16:03:26 +0100

    Develop * Fixed a prettier error for vue line endings, in .eslintrc.cjs * Added a template route for the old home page, in src/App.vue * Created a start component for the new start page, in src/components/StartComp.vue * Formated the template file component, in src/components/TheWelcome.vue * Formated the template file mini component, in src/components/WelcomeItem.vue * Replaced the template view with my newly created start page view for the / route, in src/router/index.ts * Created a start view file for the new start page.

---
+++ src/components/StartComp.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+export default {
+  name: "StartComp",
+};
+</script>
+<template>
+  <h1>Hej</h1>
+  <p>Comp</p>
+</template>
+<style></style>
---
+++ src/views/StartView.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+import StartComp from "@/components/StartComp.vue";
+
+export default {
+  name: "StartView",
+  components: { StartComp },
+};
+</script>
+<template>
+  <StartComp />
+</template>
+<style></style>
